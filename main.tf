4831a8f67e9c59d39e22caa9e868952fa86e7bfc
Doomion555 <joaogabrielbotelho4@gmail.com>
2025-12-05 17:46:34 +0000

    Adicionado a maquina que serve de DHCP server

---
+++ main.tf
@@ -0,0 +1,89 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+  required_version = ">=1.2.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "6d49d3f9-ff99-4677-9ac5-3f3f21c05299"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "dhcp-test-rg"
+  location = "switzerlandnorth"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "dhcp-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "dhcp-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "dhcp-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "dhcp-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  admin_password      = "Baguette123!"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    Environment = "DHCP-Test"
+  }
+}
+
+# Output the public IP
+output "vm_ip" {
+  value = azurerm_network_interface.nic.private_ip_address
+}
