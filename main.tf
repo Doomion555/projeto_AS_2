e0112f1b8bf04f8e954a8e3b084ddec13c1418c5
Doomion555 <joaogabrielbotelho4@gmail.com>
2025-12-08 18:19:12 +0000

    DHCP a dar IP quando se roda ansible

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "dhcp-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 # Network Interface
@@ -60,11 +60,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
 
   os_disk {
     caching              = "ReadWrite"
